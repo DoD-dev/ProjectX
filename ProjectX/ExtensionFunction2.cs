3e817eb4729e7724dc6230df0987c298b8803bc6
Độ Đoàn <cautapchung98@live.com>
2020-03-29 07:40:31 +0700

    Add ExtensionFunction1

---
+++ ProjectX/ExtensionFunction2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectX
+{
+    public static partial class ExtensionFunction
+    {
+        public static int Subtract(int x,int y)
+        {
+            return x - y;
+        }
+    }
+}
