c4c734db081478ad3245f4bb5008f29edcd6b3be
Độ Đoàn <cautapchung98@live.com>
2020-03-29 07:35:38 +0700

    Add ExtensionFunction class

---
+++ ProjectX/ExtensionFunction1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectX
+{
+    public static partial class ExtensionFunction
+    {
+        public static int Add(int x,int y)
+        {
+            return x + y;
+        }
+    }
+}
